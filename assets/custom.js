eb8714d9f63e7de51af248ce7f26350650bcb310
Scott Riley <hugs@scott.is>
2025-06-26 11:11:25 +0100

    Toggle to sticky header via JS

--- assets/custom.js
+++ assets/custom.js
@@ -1,49 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const elements = document.querySelectorAll('.brimm-blocks--split-text');
-
-  elements.forEach(function(element) {
-    const words = element.textContent.trim().split(/\s+/);
-    element.innerHTML = '';
-
-    let group = [];
-
-    const flushGroup = () => {
-      group.forEach(span => element.appendChild(span));
-      const br = document.createElement('span');
-      br.classList.add('brimm-blocks--line-break');
-      element.appendChild(br);
-      group = [];
-    };
-
-    words.forEach((word, index) => {
-      const span = document.createElement('span');
-      const wordCutoff = words.length > 4 ? 7 : 5;
-      span.textContent = word;
-      const isLong = word.length > wordCutoff;
-      span.classList.add(isLong ? 'brimm-blocks--long-word' : 'brimm-blocks--short-word');
-      group.push(span);
-
-      // Handle 3 short words
-      if (
-        group.length === 3 &&
-        group.every(w => w.classList.contains('brimm-blocks--short-word'))
-      ) {
-        flushGroup();
-      }
-
-      // Handle short + long or long + short
-      if (
-        group.length === 2 &&
-        ((group[0].classList.contains('brimm-blocks--short-word') && group[1].classList.contains('brimm-blocks--long-word')) ||
-         (group[0].classList.contains('brimm-blocks--long-word') && group[1].classList.contains('brimm-blocks--short-word')))
-      ) {
-        flushGroup();
-      }
-
-      // Flush any leftover at the end
-      if (index === words.length - 1 && group.length > 0) {
-        group.forEach(span => element.appendChild(span));
-      }
-    });
-  });
+  const header = document.querySelector('.header-wrapper')
+  const firstBlock = document.querySelector('main > div:first-child')
+  firstBlock.after(header)
+  header.classList.add('sticky')
 });
