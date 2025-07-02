fc6c2aae4bee07f3d5c805975cd7ce2a1763d29e
Scott Riley <hugs@scott.is>
2025-07-02 19:29:06 +0100

    Stop member benefits badge overlapping sold out on smaller widths

--- assets/custom.js
+++ assets/custom.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const header = document.querySelector('.header-wrapper')
   const firstBlock = document.querySelector('main > div:first-child')
-  if(firstBlock.querySelector('section:first-child').classList.contains('brimm-blocks--hero')) {
+  if(firstBlock && firstBlock.querySelector('section:first-child') && firstBlock.querySelector('section:first-child').classList.contains('brimm-blocks--hero')) {
     firstBlock.after(header)
     document.querySelector('body').classList.add('sticky-header')
     header.classList.add('sticky')
