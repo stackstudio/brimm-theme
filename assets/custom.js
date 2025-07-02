90a2f956e261b4b98cc7b802bfe5b4d9837bbb2b
Scott Riley <hugs@scott.is>
2025-07-02 16:18:47 +0100

    Nav tweaks. Only put nav mid-document if page contains a stacked hero

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const header = document.querySelector('.header-wrapper')
   const firstBlock = document.querySelector('main > div:first-child')
-  firstBlock.after(header)
-  header.classList.add('sticky')
+  console.log(document.querySelector('main > div:first-child > section:first-child'), "LOG")
+  if(firstBlock.querySelector('section:first-child').classList.contains('brimm-blocks--hero')) {
+    firstBlock.after(header)
+    header.classList.add('sticky')
+  }
 });
