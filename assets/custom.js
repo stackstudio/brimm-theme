22491a09d61006803bb3c85fd4a87a048b398905
Scott Riley <hugs@scott.is>
2025-08-15 11:46:12 +0100

    Remove custom JS (only used to toggle header between fixed/static)

--- assets/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const header = document.querySelector('.header-wrapper')
-  const firstBlock = document.querySelector('main > div:first-child')
-  if(firstBlock && firstBlock.querySelector('section:first-child') && firstBlock.querySelector('section:first-child').classList.contains('brimm-blocks--hero')) {
-    firstBlock.after(header)
-    document.querySelector('body').classList.add('sticky-header')
-    header.classList.add('sticky')
-  }
-});
